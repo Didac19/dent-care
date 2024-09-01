a9bfe5fbc7c6efb8aa4dc2ed4aba69f741ae7df5
“Didac19” <diegocastrog19@gmail.com>
2024-09-01 15:25:29 -0500

    chore: Disable prettier plugin in eslint configuration
    chore: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors();
 
   const host = configService.get<string>('HOST');
   const port = configService.get<number>('PORT');
